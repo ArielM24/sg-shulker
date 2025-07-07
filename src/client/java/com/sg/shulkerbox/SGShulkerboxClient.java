321ee5c7a836dd1cc3315643c9fb09a4b6396eab
ArielM24 <ariel_montoya15@outlook.es>
2025-07-07 11:02:49 -0600

    settings

--- src/client/java/com/sg/shulkerbox/SGShulkerboxClient.java
+++ src/client/java/com/sg/shulkerbox/SGShulkerboxClient.java
@@ -37,7 +37,7 @@ public class SGShulkerboxClient implements ClientModInitializer {
 		
 
 		key = KeyBindingHelper.registerKeyBinding(
-				new KeyBinding("open_shulker", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "Shulker Preview"));
+				new KeyBinding("Open shulker preview", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "Shulker Preview"));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.player != null) {
